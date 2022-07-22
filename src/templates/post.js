a08cd05da5aefb6c2329ec7af02fcdeb38cc0fc7
ramuskay <aurel@beerus.fr>
2022-07-22 11:58:30 -0400

    lint

--- src/templates/post.js
+++ src/templates/post.js
@@ -58,8 +58,8 @@ export default class PostTemplate extends Component {
               <div className="post-meta">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentcolor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-clock">
                   <title>clock</title>
-                  <circle cx="12" cy="12" r="10"></circle>
-                  <polyline points="12 6 12 12 16 14"></polyline>
+                  <circle cx="12" cy="12" r="10">null</circle>
+                  <polyline points="12 6 12 12 16 14">null</polyline>
                 </svg>
                 &nbsp; {post.timeToRead} min read.
               </div>
